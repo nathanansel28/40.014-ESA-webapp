b78eea92bbe69e652f5805f8f43fe17ae227f892
georgia <georgia_lau@mymail.sutd.edu.sg>
2024-07-08 00:41:51 +0800

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+
+
+// const Home = () => 
+//   <div>
+//     Home page
+//   </div>;
+
+// const About = () => 
+//   <div>
+//     About Page
+//   </div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path = '/' exact />
+      <Route path='/services' component={Services} />
+      <Route path='/products' component={Products} />
+      <Route path='/sign-up' component={SignUp} />
+      {/* <Route path = '/' element = {<About />} /> */}
+    </Routes>
+    </Router>
+ 
+    </>
   );
 }
 
 export default App;
+
+console.log('Public URL:', process.env.PUBLIC_URL);
---
+++ src/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+};
