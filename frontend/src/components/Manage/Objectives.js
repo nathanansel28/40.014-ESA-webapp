3ca0b0e616ca7f293acd5e43969138dd70f51c74
norahkla <norahkla@gmail.com>
2024-07-23 12:20:45 +0800

    added another box for results

--- frontend/src/components/Manage/Objectives.js
+++ frontend/src/components/Manage/Objectives.js
@@ -5,16 +5,24 @@ import './Objectives.css'; // Make sure the path is correct
 import { Button } from 'react-bootstrap';
 
 const Objectives = () => {
-    return ( 
+    return (
+      <div className="objectives-container">
         <div className="objective_main">
-            <p style={{color:"white"}}>Choose one objective to prioritize!</p>
-            <div className="objective_title">
-                <ObjectiveList/> 
-
-                <Button type="primary">Submit</Button>
-            </div>
+          <p style={{color:"white"}}>Choose one objective to prioritize!</p>
+          <div className="objective-title">
+            <ObjectiveList />
+            <Button type="primary">Submit</Button>
+          </div>
         </div>
-  );
-};
-
-export default Objectives;
+        <div className="objective_main">
+          <p style={{color:"white"}}>Results:</p>
+          <div className="objective-title">
+            Results... 
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Objectives;
+  
