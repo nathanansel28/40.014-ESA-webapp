c4f4571449cb0e71c1b27289b00139cb5fea6040
peckkers <104679437+peckyeoklee@users.noreply.github.com>
2024-07-27 00:16:31 +0800

    beautified the instructions section

--- frontend/src/components/Manage/Manage.js
+++ frontend/src/components/Manage/Manage.js
@@ -10,6 +10,7 @@ import { Button } from 'antd';
 import ChooseBox from '../Manage/ChooseBox';
 import SampleImage from '../../Assets/instructions_image.jpg';
 import './Manage.css';
+import Instructions from './Instructions';
 
 function Manage() {
   const [uploadedFileName, setUploadedFileName] = useState(null);
@@ -43,54 +44,10 @@ function Manage() {
 
   return (
     <>
-    {/* <Row className="instruction-section">
-  <Col md={8} style={{ textAlign: 'left', backgroundColor: 'transparent', color: 'white' }}>
-    <Card style={{ backgroundColor: 'transparent', color: 'white', border: 'none' }}>
-      <Card.Body>
-        <Card.Title style={{ textAlign: 'center', fontWeight: 'bold' }}>Instructions for Uploading CSV Files</Card.Title>
-        <Card.Text>
-          <h5>Step 1: Prepare your CSV File</h5>
-          <p>Ensure your CSV file follows the format provided in the sample file:</p>
-          <img src={SampleImage} alt="Sample CSV Format" style={{ width: '100%', marginBottom: '20px' }} />
-          <ul>
-            <li><strong>OperationID</strong>: A unique identifier for each operation.</li>
-            <li><strong>Operation</strong>: A common label for each operation.</li>
-            <li><strong>Machine</strong>: The identifier of the machine on which the operation will run.</li>
-            <li><strong>Start Date</strong>: The start date of the operation.</li>
-            <li><strong>End Date</strong>: The end date of the operation.</li>
-            <li><strong>Duration</strong>: The duration of the operation.</li>
-            <li><strong>Percent Completion</strong>: The percentage of completion for each operation.</li>
-            <li><strong>Predecessors</strong>: The predecessors constraint for each operation (Please strictly follow the format from the image above!).</li>
-          </ul>
-          <h5>Step 2: Upload the CSV File</h5>
-          <ul>
-            <li>Navigate to the upload section on the platform.</li>
-            <li>Click on the 'Upload file' button.</li>
-            <li>Select the CSV file from your device that matches the format described above.</li>
-          </ul>
-          <h5>Step 3: Select your minimization objective</h5>
-          <ul>
-            <li>After successfully uploading your CSV file you will be prompted to choose the objective you wish to minimize. The minimization objectives include:</li>
-            <ul>
-              <li>Minimize makespan</li>
-              <li>Minimize WIP holding cost</li>
-              <li>Minimize runtime</li>
-              <li>Minimize Number of Tardy jobs</li>
-            </ul>
-          </ul>
-          <h5>Step 4: Generating Gantt Chart</h5>
-          <p>Once your CSV file is uploaded and your minimization objective is selected, the Gantt Chart will be automatically generated. Please review the Gantt Chart to ensure it accurately represents your schedule.</p>
-          <h5>Additional Notes:</h5>
-          <ul>
-            <li>Ensure all dates, duration, and percent completion are in the correct unit of measure (i.e. Short Date, Number, and Number respectively).</li>
-            <li>Double-check for any missing or incorrect data before uploading.</li>
-          </ul>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </Col>
-</Row> */}
-
+    <div className="central-container">
+      <Instructions />
+    </div>
+    
       <Container fluid>
         <Row>
           <Col>
